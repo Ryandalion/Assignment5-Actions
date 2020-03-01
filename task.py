dd3d700e7839630d7986fdcbf358a7493b159c5d
Ryan Yun <Ryandalion@github.com>
2020-02-29 23:06:17 -0800

    Added test_area unit test

--- task.py
+++ task.py
@@ -1,2 +1,6 @@
 def firstrun():
     return "success"
+
+def compute_area(radius):
+    area = 3.14 * radius * radius
+    return area
--- tests.py
+++ tests.py
@@ -1,16 +1,20 @@
 import unittest
-import task
+import task as task_test
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         expected = "success"
-        self.assertEqual(expected, task.firstrun())
+        self.assertEqual(expected, task_test.firstrun())
 
     def test2(self):
         expected = "failure"
-        self.assertNotEqual(expected, task.firstrun())
+        self.assertNotEqual(expected, task_test.firstrun())
+
+    def test_area(self):
+        result = task_test.compute_area(5);
+        self.assertEqual(78.5, result);
 
 
 if __name__ == '__main__':
