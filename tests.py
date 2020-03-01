8b72c41b51a5d06424ec6d780797d72ce792eebf
Ryan Yun <Ryandalion@github.com>
2020-02-29 23:17:09 -0800

    Retrieve first last element

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ import task as task_test
 
 class TestCase(unittest.TestCase):
 
+
     def test1(self):
         expected = "success"
         self.assertEqual(expected, task_test.firstrun())
@@ -17,7 +18,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(78.5, result)
 
     def test_first_last(self):
-        self.assertEqual([1, 10], task_test.compute_first_last([1,2,3,4,5,6,7,8,9,10]))
+        self.assertEqual([1, 10], task_test.compute_first_last([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 if __name__ == '__main__':
     unittest.main()
